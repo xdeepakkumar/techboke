650f8ba95c39d5907ce9bf339f9b3841570313c3
deepakkumar007 <deepak.kumar@gigaforce.io>
2023-08-13 02:13:17 +0530

    initial commit

---
+++ src/main/java/com/techboke/TechbokeApplication.java
@@ -0,0 +1,13 @@
+package com.techboke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbokeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techboke/TechbokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techboke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
